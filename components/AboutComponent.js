206e54f106f70226358c56b0a3b4d84edcc44c50
Christina Roufosse <roufosse.christina@gmail.com>
2019-07-13 11:51:10 -0700

    Text Change

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -13,10 +13,13 @@ function History() {
         world fusion cuisine that can be found nowhere else, it enjoys patronage
         from the A-list clientele in Hong Kong. Featuring four of the best
         three-star Michelin chefs in the world, you never know what will arrive
-        on your plate the next time you visit us. The restaurant traces its
-        humble beginnings to The Frying Pan, a successful chain started by our
-        CEO, Mr. Peter Pan, that featured for the first time the world's best
-        cuisines in a pan.
+        on your plate the next time you visit us.
+        <Text />
+        <Text>
+          The restaurant traces its humble beginnings to The Frying Pan, a
+          successful chain started by our CEO, Mr. Peter Pan, that featured for
+          the first time the world's best cuisines in a pan.
+        </Text>
       </Text>
     </Card>
   );
