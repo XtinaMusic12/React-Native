78958707a9ee9ba060bd028dda8d7ad071130ecd
Christina Roufosse <roufosse.christina@gmail.com>
2019-07-13 12:10:16 -0700

    Last Styling About

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -7,19 +7,21 @@ import { ListItem } from "react-native-elements";
 function History() {
   return (
     <Card title="Our History">
-      <Text>
+      <Text style={{ textAlign: "justify" }}>
         Started in 2010, Ristorante con Fusion quickly established itself as a
         culinary icon par excellence in Hong Kong. With its unique brand of
         world fusion cuisine that can be found nowhere else, it enjoys patronage
-        from the A-list clientele in Hong Kong. Featuring four of the best
-        three-star Michelin chefs in the world, you never know what will arrive
-        on your plate the next time you visit us.
-        <Text />
-        <Text>
-          The restaurant traces its humble beginnings to The Frying Pan, a
-          successful chain started by our CEO, Mr. Peter Pan, that featured for
-          the first time the world's best cuisines in a pan.
-        </Text>
+        from the A-list clientele in Hong Kong.
+      </Text>
+      <Text style={{ textAlign: "justify" }}>
+        Featuring four of the best three-star Michelin chefs in the world, you
+        never know what will arrive on your plate the next time you visit us.
+      </Text>
+      <Text />
+      <Text style={{ textAlign: "justify" }}>
+        The restaurant traces its humble beginnings to The Frying Pan, a
+        successful chain started by our CEO, Mr. Peter Pan, that featured for
+        the first time the world's best cuisines in a pan.
       </Text>
     </Card>
   );
