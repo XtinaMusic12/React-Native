bf5e78f76f57f81b9d55765c81f1e2f892c0b6e8
Christina Roufosse <roufosse.christina@gmail.com>
2019-07-19 20:49:50 -0700

    Activity Indicator

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -4,6 +4,7 @@ import { FlatList } from "react-native";
 import { Tile } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import { Loading } from "./LoadingComponent";
 
 const mapStateToProps = state => {
   return {
@@ -32,13 +33,23 @@ class Menu extends Component {
 
     const { navigate } = this.props.navigation;
 
-    return (
-      <FlatList
-        data={this.props.dishes.dishes}
-        renderItem={renderMenuItem}
-        keyExtractor={item => item.id.toString()}
-      />
-    );
+    if (this.props.dishes.isLoading) {
+      return <Loading />;
+    } else if (this.props.dishes.errMess) {
+      return (
+        <View>
+          <Text>{props.dishes.errMess}</Text>
+        </View>
+      );
+    } else {
+      return (
+        <FlatList
+          data={this.props.dishes.dishes}
+          renderItem={renderMenuItem}
+          keyExtractor={item => item.id.toString()}
+        />
+      );
+    }
   }
 }
 
